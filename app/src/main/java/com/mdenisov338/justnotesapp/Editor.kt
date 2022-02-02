d6952af8d9e687de726261b45da40070c62c8108
mdenisov <mdenisov.work@gmail.com>
2022-02-02 14:39:46 +0400

    v1.0 release!

--- app/src/main/java/com/mdenisov338/justnotesapp/Editor.kt
+++ app/src/main/java/com/mdenisov338/justnotesapp/Editor.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
 import android.R
+import android.net.Uri
 import android.view.View
 
 import android.widget.EditText
@@ -15,19 +16,38 @@ import java.lang.StringBuilder
 
 
 class Editor : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_editor)
+        setContentView(com.mdenisov338.justnotesapp.R.layout.activity_editor)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
-
     }
 
     fun exisave(view: View) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        finish()
     }
+
+    fun vk_a(view: View){
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/mdenisovapps"))
+        startActivity(i)
+    }
+
+    fun tg_a(view: View){
+
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/mdenisovapps"))
+                startActivity(i)
+    }
+
+    fun gh_a(view: View){
+
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/mdenisov338/JustNotes"))
+                startActivity(i)
+    }
+
+    fun site_a(view: View){
+
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://mdenisovapps.carrd.co/"))
+                startActivity(i)
+    }
+
 }
 
--- app/src/main/java/com/mdenisov338/justnotesapp/MainActivity.kt
+++ app/src/main/java/com/mdenisov338/justnotesapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.mdenisov338.justnotesapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +15,58 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        loadData()
+
+
+        val fab: View = findViewById(R.id.fab)
+
+        val fab2: View = findViewById(R.id.fab2)
+
+
+        fab.setOnClickListener {
+            val intent = Intent(this, Editor::class.java)
+            startActivity(intent)
+            }
+
+        fab2.setOnClickListener {
+            saveData()
+
+        }
+
+
+    }
+
+    private fun saveData(){
+        val thisnote = findViewById<EditText>(R.id.thisnote)
+
+
+        val note: String = thisnote.text.toString()
+        thisnote.text = Editable.Factory.getInstance().newEditable(note)
+
+        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.apply(){
+            putString("STRING_KEY", note)
+
+        }.apply()
+
+        Toast.makeText(this,"Сохранено!", Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun loadData(){
+        val thisnote = findViewById<EditText>(R.id.thisnote)
+        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedString = sharedPreferences.getString("STRING_KEY", null)
+
+        thisnote.text = Editable.Factory.getInstance().newEditable(savedString)
+
     }
 
 
-    fun createNote(view: android.view.View) {
-        val intent = Intent(this, Editor::class.java)
-        startActivity(intent)
 
-
+    override fun onBackPressed(){
+        finish()
     }
 }
